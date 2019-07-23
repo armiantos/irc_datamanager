6d45735aeb86293010af5140f59f3ce0211874c8
armiantos <armiantos@gmail.com>
2019-07-23 08:40:37 -0600

    added status reporter class

---
+++ irc_datamanager/HelperClasses/Status.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace irc_datamanager.HelperClasses
+{
+    public class Status
+    {
+        public delegate void StatusUpdateEventHandler(string message, int priority);
+
+        public static event StatusUpdateEventHandler OnUpdate;
+
+        public static void Update(string message, int priority)
+        {
+            OnUpdate(message, priority);
+        }
+    }
+}
