1df61c66469795d6c82a749f80137b34dcf60822
Armianto Sumitro <armiantos@gmail.com>
2019-07-22 20:25:52 -0600

    Started working on OPC DA data source

---
+++ irc_datamanager/ViewModels/OpcDaViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace irc_datamanager.ViewModels
+{
+    class OpcDaViewModel : ViewModel
+    {
+        public override string ViewModelName
+        {
+            get
+            {
+                return "OPC DA";
+            }
+        }
+
+    }
+}
