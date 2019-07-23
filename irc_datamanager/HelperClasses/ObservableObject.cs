438c3bcc266b7a4a80c45cc2fc9411f6f8a8504c
Armianto Sumitro <armiantos@gmail.com>
2019-07-22 20:25:41 -0600

    Added application ViewModel and INotifyPropertyChanged helper

---
+++ irc_datamanager/HelperClasses/ObservableObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace irc_datamanager.HelperClasses
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(object sender, string propertyName)
+        {
+            PropertyChanged?.Invoke(sender,
+                new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ irc_datamanager/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,60 @@
+﻿using irc_datamanager.HelperClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace irc_datamanager.ViewModels
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        private ViewModel currentSourceViewModel;
+        private ViewModel currentSinkViewModel;
+        private ViewModel operationsViewModel;
+
+        public MainWindowViewModel()
+        {
+            
+        }
+
+        public ViewModel CurrentSourceViewModel
+        {
+            get
+            {
+                return currentSourceViewModel;
+            }
+            set
+            {
+                currentSourceViewModel = value;
+                OnPropertyChanged(this, "CurrentSourceViewModel");
+            }
+        }
+
+        public ViewModel CurrentSinkViewModel
+        {
+            get
+            {
+                return currentSinkViewModel;
+            }
+            set
+            {
+                currentSinkViewModel = value;
+                OnPropertyChanged(this, "CurrentSinkViewModel");
+            }
+        }
+
+        public ViewModel OperationViewModel
+        {
+            get
+            {
+                return operationsViewModel;
+            }
+            set
+            {
+                operationsViewModel = value;
+                OnPropertyChanged(this, "OperationViewModel");
+            }
+        }
+    }
+}
