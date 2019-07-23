3d1bf5fa4bea0c3d70a92bd340270a3d8cfb3800
Armianto Sumitro <armiantos@gmail.com>
2019-07-23 08:14:16 -0600

    implemented icommand wrapper

---
+++ irc_datamanager/HelperClasses/CommandWrapper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace irc_datamanager.HelperClasses
+{
+    class CommandWrapper : ICommand
+    {
+        private Action<object> execute;
+        private Predicate<object> canExecute;
+
+        public CommandWrapper(Action<object> execute) : this(execute, null) { }
+
+        public CommandWrapper(Action<object> execute, Predicate<object> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null ? true : canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
