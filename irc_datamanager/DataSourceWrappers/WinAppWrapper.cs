7b825f1ed0baaac9e542cf21d41b9eef53367b4b
armiantos <armiantos@gmail.com>
2019-07-23 13:53:28 -0600

    winappwrapper can list window titles

--- irc_datamanager/DataSourceWrappers/WinAppWrapper.cs
+++ irc_datamanager/DataSourceWrappers/WinAppWrapper.cs
@@ -9,22 +9,50 @@ namespace irc_datamanager.DataSourceWrappers
 {
     public class WinAppWrapper
     {
-
+        #region delegates
 
         public delegate bool CallBackPtr(int hwnd, int lParam);
-
         private delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
 
+        #endregion
+
+        #region dll imports
+
+        // used to navigate top level windows
         [DllImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
         static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
 
-        [DllImport("user32.dll", SetLastError = true)]
-        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        // used to get window caption/title
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        // help to get window caption/title
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern int GetWindowTextLength(IntPtr hWnd);
+
+        // used to find window from cursor
+        [DllImport("user32.dll")]
+        static extern IntPtr WindowFromPoint(System.Drawing.Point p);
+
+        #endregion
+
+        #region custom wrappers
+
+        public static string GetText(IntPtr hWnd)
+        {
+            // Allocate correct string length first
+            int length = GetWindowTextLength(hWnd);
+            StringBuilder sb = new StringBuilder(length + 1);
+            GetWindowText(hWnd, sb, sb.Capacity);
+            return sb.ToString();
+        }
 
         public static bool GetWindowsCallback(IntPtr hWnd, IntPtr lParam)
         {
-            Console.WriteLine(hWnd);
+            string wCaption = GetText(hWnd);
+            if (!string.IsNullOrEmpty(wCaption))
+                Console.WriteLine($"{hWnd} : {wCaption}");
             return true;
         }
 
@@ -33,5 +61,9 @@ namespace irc_datamanager.DataSourceWrappers
             EnumWindowsProc callback = new EnumWindowsProc(GetWindowsCallback);
             EnumWindows(callback, IntPtr.Zero);
         }
+
+        #endregion
+
+
     }
 }
