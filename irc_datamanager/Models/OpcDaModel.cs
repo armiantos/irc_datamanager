c52cc8706e2cb5488fb01144596b5d97472c5590
Armianto Sumitro <armiantos@gmail.com>
2019-07-23 08:15:01 -0600

    added opcda data model

---
+++ irc_datamanager/Models/OpcDaModel.cs
@@ -0,0 +1,94 @@
+﻿using irc_datamanager.HelperClasses;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace irc_datamanager.Models
+{
+    public class OpcDaModel : ObservableObject
+    {
+        private string host;
+        private DataTable itemsTable;
+        private DataView itemsView;
+        private string itemsViewVisibility;
+        private string opcServersVisibility;
+
+        public OpcDaModel()
+        {
+            OpcServersVisibility = "Hidden";
+            ItemsViewVisibility = "Hidden";
+        }
+
+        public string Host
+        {
+            get
+            {
+                return host;
+            }
+            set
+            {
+                host = value;
+                OnPropertyChanged("Host");
+            }
+        }
+
+        public ObservableCollection<string> OpcServers { get; set; }
+
+        public DataView ItemsView
+        {
+            get
+            {
+                return itemsView;
+            }
+            set
+            {
+                itemsView = value;
+                OnPropertyChanged("ItemsView");
+            }
+        }
+
+        public DataTable ItemsTable
+        {
+            get
+            {
+                return itemsTable;
+            }
+            set
+            {
+                itemsTable = value;
+                OnPropertyChanged("ItemTable");
+            }
+        }
+
+        public string OpcServersVisibility
+        {
+            get
+            {
+                return opcServersVisibility;
+            }
+            set
+            {
+                opcServersVisibility = value;
+                OnPropertyChanged("OpcServersVisibility");
+            }
+        }
+
+        public string ItemsViewVisibility
+        {
+            get
+            {
+                return itemsViewVisibility;
+            }
+            set
+            {
+                itemsViewVisibility = value;
+                OnPropertyChanged("ItemsViewVisibility");
+            }
+        }
+
+    }
+}
