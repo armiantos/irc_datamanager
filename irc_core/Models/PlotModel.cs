d861d686260e01cd6f04e209f7f91d6017a2498b
armiantos <armiantos@gmail.com>
2019-08-01 09:53:19 -0600

    personalized oxyplot oclorscheme

--- irc_core/Models/PlotModel.cs
+++ irc_core/Models/PlotModel.cs
@@ -1,10 +1,12 @@
 ﻿using OxyPlot;
+using System.Collections.Generic;
 
 namespace irc_core.Models
 {
     public class PlotModel : DataModel
     {
         private OxyPlot.PlotModel model;
+
 
         public OxyPlot.PlotModel Model
         {
@@ -13,6 +15,15 @@ namespace irc_core.Models
                 if (model == null)
                 {
                     model = new OxyPlot.PlotModel();
+                    model.DefaultColors = new List<OxyColor>
+                    {
+                        OxyColor.FromRgb(166, 8, 84),
+                        OxyColor.FromRgb(166, 8, 8),
+                        OxyColor.FromRgb(66, 66, 66),
+                        OxyColor.FromRgb(87, 8, 8),
+                        OxyColor.FromRgb(61, 59, 71),
+                        OxyColor.FromRgb(105, 105, 105),
+                    };
                 }
                 return model;
             }
